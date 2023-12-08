c6dacbece294ff2df9d84d301e351dcaad76724a
jackyef <jacky.efendi1@gmail.com>
2023-12-08 15:30:29 +0700

    Fix error

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,6 +1,6 @@
-import clsx from "clsx"
-import { twMerge } from "tailwind-merge"
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export const cx = (...classNames: (Parameters<typeof clsx>)) => {
+export const cx = (...classNames: Parameters<typeof clsx>) => {
   return twMerge(clsx(...classNames))
 }
